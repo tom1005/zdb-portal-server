4ba7e403e4e775a6870fdb6c895ac03f2b6cb63e
Administrator <Administrator@SKCC16D00440.SKCC.NET>
2018-07-23 11:32:25 +0900

    2018-07-23 스케줄 실행전 서비스 유효성 체크 추가

--- src/main/java/com/zdb/core/repository/ScheduleEntityRepository.java
+++ src/main/java/com/zdb/core/repository/ScheduleEntityRepository.java
@@ -14,38 +14,48 @@ import com.zdb.core.domain.ScheduleEntity;
 
 /**
  * Repository for ZDBEntity
- * 
+ *
  * @author 06919
  *
  */
 @Repository
 public interface ScheduleEntityRepository extends CrudRepository<ScheduleEntity, String> {
-	
-	@Query("select t from ScheduleEntity t where scheduleId=:scheduleId" )
-	ScheduleEntity findSchedule(@Param("scheduleId") String scheduleId);
 
-	@Query("select t from ScheduleEntity t "
-			+ "where namespace=:namespace "
-			+ "and serviceType=:serviceType "
-			+ "and serviceName=:serviceName" )
-	ScheduleEntity findScheduleByName(@Param("namespace") String namespace
-			, @Param("serviceType") String serviceType
-			, @Param("serviceName") String serviceName);
-	
-	@Query("select t from ScheduleEntity t where startTime=:startTime order by registerDate asc" )
-	List<ScheduleEntity> findCurrentSchedule(@Param("startTime") String startTime);
-	
-	@Query("select t from ScheduleEntity t" )
-	List<ScheduleEntity> findAll();
+        @Query("select t from ScheduleEntity t where scheduleId=:scheduleId" )
+        ScheduleEntity findSchedule(@Param("scheduleId") String scheduleId);
 
-	@Modifying(clearAutomatically = true)
-	@Transactional
-	@Query("UPDATE ScheduleEntity t SET "
-			+ "t.startTime=:startTime"
-			+ ", t.storePeriod=:storePeriod"
-			+ ", t.useYn=:useYn WHERE t.scheduleId=:scheduleId")
-	int modify(@Param("startTime") String startTime
-			, @Param("storePeriod") int storePeriod
-			, @Param("useYn") String useYn
-			, @Param("scheduleId") String scheduleId);
+        @Query("select t from ScheduleEntity t "
+                        + "where namespace=:namespace "
+                        + "and serviceType=:serviceType "
+                        + "and serviceName=:serviceName" )
+        ScheduleEntity findScheduleByName(@Param("namespace") String namespace
+                        , @Param("serviceType") String serviceType
+                        , @Param("serviceName") String serviceName);
+
+        @Query("select t from ScheduleEntity t where startTime=:startTime and deleteYn='N' order by registerDate asc" )
+        List<ScheduleEntity> findCurrentSchedule(@Param("startTime") String startTime);
+
+        @Query("select t from ScheduleEntity t" )
+        List<ScheduleEntity> findAll();
+
+        @Modifying(clearAutomatically = true)
+        @Transactional
+        @Query("UPDATE ScheduleEntity t SET"
+                        + " t.startTime=:startTime"
+                        + ", t.storePeriod=:storePeriod"
+                        + ", t.useYn=:useYn "
+                        + "WHERE t.scheduleId=:scheduleId")
+        int modify(@Param("startTime") String startTime
+                        , @Param("storePeriod") int storePeriod
+                        , @Param("useYn") String useYn
+                        , @Param("scheduleId") String scheduleId);
+
+        @Modifying(clearAutomatically = true)
+        @Transactional
+        @Query("UPDATE ScheduleEntity t SET"
+                        + " t.useYn='N'"
+                        + ", t.deleteYn='Y'"
+                        + ", t.deleteDate=now() "
+                        + "WHERE t.scheduleId=:scheduleId")
+        int modify2Delete(@Param("scheduleId") String scheduleId);
 }
