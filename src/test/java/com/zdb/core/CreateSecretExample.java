75f17d7c424912a26bb40a58e32f05fe0d9f04a5
a06919 <pns@sk.com>
2018-11-07 14:44:05 +0900

    예제 추가(secret 생성)

--- src/test/java/com/zdb/core/CreateSecretExample.java
+++ src/test/java/com/zdb/core/CreateSecretExample.java
@@ -3,14 +3,16 @@ package com.zdb.core;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.fabric8.kubernetes.client.AutoAdaptableKubernetesClient;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
-public class CredentialsExample {
+public class CreateSecretExample {
 
-	private static final Logger logger = LoggerFactory.getLogger(CredentialsExample.class);
+	private static final Logger logger = LoggerFactory.getLogger(CreateSecretExample.class);
 
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -23,9 +25,25 @@ public class CredentialsExample {
 				.withNamespace("default")
 				.build();
 
-		try (final KubernetesClient client = new AutoAdaptableKubernetesClient(config)) {
+		try (final DefaultKubernetesClient client = new DefaultKubernetesClient(config)) {
+			String namespace = "zdb-test";
+			String secretName = "zdb-system-secret";
 			
-			log("Received pods", client.pods().list());
+			Secret s = client.inNamespace(namespace).secrets().withName(secretName).get();
+			if(s == null) {
+				Secret secret = client.inNamespace("zdb-system").secrets().withName(secretName).get();
+				
+				ObjectMeta metadata = new ObjectMeta();
+				metadata.setNamespace(namespace);
+				metadata.setName(secretName);
+				secret.setMetadata(metadata);
+				
+				client.inNamespace(namespace).secrets().create(secret);
+				
+			} else {
+				System.out.println("exist secret." + namespace);
+			}
+			
 
 		} catch (Exception e) {
 			e.printStackTrace();
