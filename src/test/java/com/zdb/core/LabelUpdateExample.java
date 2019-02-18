0995fdb695e297f51b50e92d06c784d253a2565e
a06919 <pns@sk.com>
2019-02-18 13:24:52 +0900

    label update example

---
+++ src/test/java/com/zdb/core/LabelUpdateExample.java
@@ -0,0 +1,40 @@
+package com.zdb.core;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.zdb.core.util.K8SUtil;
+
+import io.fabric8.kubernetes.api.model.extensions.DoneableStatefulSet;
+import io.fabric8.kubernetes.api.model.extensions.StatefulSet;
+import io.fabric8.kubernetes.api.model.extensions.StatefulSetBuilder;
+import io.fabric8.kubernetes.api.model.extensions.StatefulSetList;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.RollableScalableResource;
+
+public class LabelUpdateExample {
+
+	private static final Logger logger = LoggerFactory.getLogger(LabelUpdateExample.class);
+
+	public static void main(String[] args) {
+		try {
+			MixedOperation<StatefulSet, StatefulSetList, DoneableStatefulSet, RollableScalableResource<StatefulSet, DoneableStatefulSet>> statefulSets = K8SUtil.kubernetesClient().inNamespace("zdb-test2").apps()
+					.statefulSets();
+			StatefulSet sts = statefulSets.withName("zdb-test2-mha-mariadb-master").get();
+
+			Map<String, String> labels = sts.getMetadata().getLabels();
+			labels.put("zdb-failover-enable", "false");
+
+			StatefulSetBuilder newSts = new StatefulSetBuilder(sts);
+			StatefulSet newSvc = newSts.editMetadata().withLabels(labels).endMetadata().build();
+
+			statefulSets.createOrReplace(newSvc);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
