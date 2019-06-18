1ebb2e220e82cccae3a355444229461b7be1aba9
박남수(Park Nam-Soo)/플랫폼Operation그룹/SK <pns@sk.com>
2019-06-18 16:21:21 +0900

    service lb 의 external-ip 가 존재하지 않는 경우에 대한 처리

--- src/main/java/com/zdb/core/domain/ConnectionInfo.java
+++ src/main/java/com/zdb/core/domain/ConnectionInfo.java
@@ -47,6 +47,9 @@ public class ConnectionInfo {
 	public String getConnectionString(Connection conn) {
 		String connectionString = new String();
 
+		if("unknown".equals(conn.getIpAddress())) {
+			return "EXTERNAL-IP가 존재하지 않습니다. 관리자에게 문의하세요.";
+		}
 	    switch (dbType) {
 	    case MariaDB:
 	    	// jdbc:mariadb://169.56.77.196:3306/zdb
@@ -64,7 +67,9 @@ public class ConnectionInfo {
 	
 	public String getConnectionLine(Connection conn) {
 		String connectionLine = new String();
-
+		if("unknown".equals(conn.getIpAddress())) {
+			return "EXTERNAL-IP가 존재하지 않습니다. 관리자에게 문의하세요.";
+		}
 	    switch (dbType) {
 	    case MariaDB:
 	    	connectionLine = String.format("mysql -u admin -p[password] --host %s --port %d", conn.getIpAddress(), conn.getPort());
