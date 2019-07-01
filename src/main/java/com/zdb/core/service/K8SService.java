6f01f5404cde89320d7e22a9f5af7f3f1121e32b
lsc <lsc@10.250.64.113>
2019-07-01 10:25:45 +0900

    merge

--- src/main/java/com/zdb/core/service/K8SService.java
+++ src/main/java/com/zdb/core/service/K8SService.java
@@ -655,9 +655,6 @@ public class K8SService {
 					}
 				} else if ("mariadb".equals(app)) {
 					String component = labels.get("component");
-					
-					// TODO
-					// Slave - ReplicationStatus: so.setReplicationStatus(slaveSrtatus)
 					if ("master".equals(component)) {
 						isMaster = true;
 					} else {
